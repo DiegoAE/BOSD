f7ef64a54e9a6ee755e82d176e9f5b6ed372a941
diego <aleagues@gmail.com>
2017-12-12 12:05:36 -0500

    Alpha and beta computations ready with toy HMM example.

--- examples/FB_example.cpp
+++ examples/FB_example.cpp
@@ -1,19 +1,39 @@
 #include <ForwardBackward.hpp>
 #include <iostream>
+#include <cmath>
 
 using namespace arma;
 using namespace std;
 
+vec sampleFromDummyHMM(int n, int k) {
+    vec A = randi<vec>(n, distr_param(0, k - 1));
+    // cout << A << endl;
+    vec B = randn<vec>(n);
+    return A + B;
+}
+
+double gaussianpdf(double x, double mu, double sigma) {
+    double ret = exp(((x - mu)*(x - mu)) / (-2*sigma*sigma));
+    ret = ret / (sqrt(2 * M_PI) * sigma);
+    return ret;
+}
+
 int main() {
-    int nobs = 31;
-    int nstates = 1;
-    int ndurations = 3;
-    int min_duration = 4;
+    int nobs = 10;
+    int nstates = 4;
+    int ndurations = 1;
+    int min_duration = 1;
     mat transition(nstates, nstates, fill::eye);
-    vec pi = {1};
+    transition.fill(1.0/nstates);
+    vec pi(nstates, fill::eye);
+    pi.fill(1.0/nstates);
     mat durations(nstates, ndurations, fill::zeros);
     durations.col(0) = ones<vec>(nstates);
     cube pdf(nstates, nobs, ndurations, fill::zeros);
+    vec toy_obs = sampleFromDummyHMM(nobs, nstates);
+    for(int i = 0; i < nstates; i++)
+        for(int t = 0; t < nobs; t++)
+            pdf(i, t, 0) = gaussianpdf(toy_obs(t), i, 1.0);
     mat alpha(nstates, nobs, fill::zeros);
     mat beta(nstates, nobs, fill::zeros);
     mat alpha_s(nstates, nobs, fill::zeros);
@@ -22,5 +42,11 @@ int main() {
             min_duration, nobs);
     cout << alpha << endl;
     cout << beta << endl;
+    cout << "Sums columns" << endl;
+    cout << sum(alpha, 0) << endl;
+    cout << sum(beta, 0) << endl;
+    cout << "Sums rows" << endl;
+    cout << sum(alpha, 1) << endl;
+    cout << sum(beta, 1) << endl;
     return 0;
 }
