d47ea9676a484d790bf6d51c1884b7b22e3ed0f2
diego <aleagues@gmail.com>
2017-12-19 10:12:57 -0500

    Creating a class for sampling observations from an HSMM. The Viterbi path reconstruction was also refactored and tested, it works fine with sequence lengths up to ~1500 observations (this is without logs).

--- examples/FB_example.cpp
+++ examples/FB_example.cpp
@@ -1,22 +1,81 @@
 #include <ForwardBackward.hpp>
 #include <iostream>
+#include <cassert>
 #include <cmath>
+#include <vector>
 
 using namespace arma;
 using namespace std;
 
-vec sampleFromDummyHMM(int n, int k, vec& states) {
-    states = randi<vec>(n, distr_param(0, k - 1));
-    cout << "Hidden states" << endl;
-    cout << states << endl;
-    vec B = randn<vec>(n) * 0.1;
-    cout << "Obs" << endl;
-    cout << states + B << endl;
-    return states + B;
+
+int sampleFromCategorical(rowvec pmf) {
+    rowvec prefixsum(pmf);
+    for(int i = 1; i < pmf.n_elem; i++)
+        prefixsum(i) += prefixsum(i - 1);
+    return lower_bound(prefixsum.begin(), prefixsum.end(), randu()) -
+            prefixsum.begin();
 }
 
+class DummyHSMM {
+    public:
+        DummyHSMM(mat transition, vec pi, vec duration, int min_duration) {
+            transition_ = transition;
+            pi_ = pi;
+            duration_ = duration;
+            min_duration_ = min_duration;
+        }
 
-void viterbiPath(const imat& psi_d, const imat& psi_s, const mat& delta) {
+        int getNumberOfStates() {
+            return transition_.n_rows;
+        }
+
+        rowvec sampleFromState(int state, int len) {
+            return randn<vec>(len) * 0.1 + state;
+        }
+
+        mat sampleSegments(int nsegments, ivec& hiddenStates,
+                ivec& hiddenDurations) {
+            assert(nsegments >= 1);
+
+            // Generating states sequence.
+            ivec states(nsegments);
+            states(0) = sampleFromCategorical(pi_.t());
+            for(int i = 1; i < nsegments; i++) {
+                rowvec nstateDist = transition_.row(states(i - 1));
+                states(i) = sampleFromCategorical(nstateDist);
+            }
+
+            // Generating durations from states.
+            int sampleSequenceLength = 0;
+            ivec durations(nsegments);
+            for(int i = 0; i < nsegments; i++) {
+                int currentDuration = sampleFromCategorical(
+                        duration_.row(states(i))) + min_duration_;
+                durations(i) = currentDuration;
+                sampleSequenceLength += currentDuration;
+            }
+
+            // Generating samples
+            mat samples(1, sampleSequenceLength, fill::zeros);
+            int idx = 0;
+            for(int i = 0; i < nsegments; i++) {
+                rowvec currSample = sampleFromState(states(i), durations(i));
+                samples.cols(idx, idx + durations(i) - 1) = currSample;
+                idx += durations(i);
+            }
+            hiddenStates = states;
+            hiddenDurations = durations;
+            return samples;
+        }
+
+        mat transition_;
+        vec pi_;
+        vec duration_;
+        int min_duration_;
+};
+
+void viterbiPath(const imat& psi_d, const imat& psi_s, const mat& delta,
+        ivec& hiddenStates, ivec& hiddenDurations) {
     int nstates = delta.n_rows;
     int nobs = delta.n_cols;
     int curr_state = 0;
@@ -24,14 +83,24 @@ void viterbiPath(const imat& psi_d, const imat& psi_s, const mat& delta) {
     for(int i = 0; i < nstates; i++)
         if (delta(i, curr_obs_idx) > delta(curr_state, curr_obs_idx))
             curr_state = i;
+    vector<int> statesSeq, durationSeq;
     while(curr_obs_idx >= 0) {
         int d = psi_d(curr_state, curr_obs_idx);
         int next_state = psi_s(curr_state, curr_obs_idx);
-        cout << "[" << curr_obs_idx - d + 1 << ", " << curr_obs_idx << "] -> " <<
-                curr_state << endl;
+
+        // Making sure that the Viterbi algorithm ran correctly.
+        assert(d >= 1);
+
+        // current segment: [curr_obs_idx - d + 1, curr_obs_idx] -> curr_state.
+        statesSeq.push_back(curr_state);
+        durationSeq.push_back(d);
         curr_obs_idx = curr_obs_idx - d;
         curr_state = next_state;
     }
+    reverse(statesSeq.begin(), statesSeq.end());
+    reverse(durationSeq.begin(), durationSeq.end());
+    hiddenStates = conv_to<ivec>::from(statesSeq);
+    hiddenDurations = conv_to<ivec>::from(durationSeq);
 }
 
 double gaussianpdf(double x, double mu, double sigma) {
@@ -52,44 +121,71 @@ int main() {
     mat durations(nstates, ndurations, fill::zeros);
     durations.col(0) = ones<vec>(nstates);
     cube pdf(nstates, nobs, ndurations, fill::zeros);
-    vec toy_states;
-    vec toy_obs = sampleFromDummyHMM(nobs, nstates, toy_states);
+
+    DummyHSMM dhsmm(transition, pi, durations, min_duration);
+    ivec hiddenStates, hiddenDurations;
+    mat samples = dhsmm.sampleSegments(nobs, hiddenStates, hiddenDurations);
+
+    cout << "Generated samples" << endl;
+    cout << samples << endl;
+    cout << "Generated states and durations" << endl;
+    cout << join_horiz(hiddenStates, hiddenDurations) << endl;
+
     for(int i = 0; i < nstates; i++)
         for(int t = 0; t < nobs; t++)
-            pdf(i, t, 0) = gaussianpdf(toy_obs(t), i, 0.1);
+            pdf(i, t, 0) = gaussianpdf(samples(0, t), i, 0.1);
+
     mat alpha(nstates, nobs, fill::zeros);
     mat beta(nstates, nobs, fill::zeros);
     mat alpha_s(nstates, nobs, fill::zeros);
     mat beta_s(nstates, nobs, fill::zeros);
     FB(transition, pi, durations, pdf, alpha, beta, alpha_s, beta_s,
             min_duration, nobs);
-//     cout << alpha << endl;
-//     cout << beta << endl;
-//     cout << "Sums columns" << endl;
-//     cout << sum(alpha, 0) << endl;
-//     cout << sum(beta, 0) << endl;
-//     cout << "Sums rows" << endl;
-//     cout << sum(alpha, 1) << endl;
-//     cout << sum(beta, 1) << endl;
+    cout << "Alpha" << endl;
+    cout << alpha << endl;
+    cout << "Beta" << endl;
+    cout << beta << endl;
+    cout << "Alpha normalized" << endl;
+    mat alpha_normalized(alpha);
+    for(int i = 0; i < alpha.n_rows; i++)
+        alpha_normalized.row(i) /= sum(alpha, 0);
+    cout << alpha_normalized << endl;
+    cout << "Sums columns" << endl;
+    cout << sum(alpha, 0) << endl;
+    cout << sum(beta, 0) << endl;
+    cout << "Sums rows" << endl;
+    cout << sum(alpha, 1) << endl;
+    cout << sum(beta, 1) << endl;
     imat psi_duration(nstates, nobs, fill::zeros);
     imat psi_state(nstates, nobs, fill::zeros);
     mat delta(nstates, nobs, fill::zeros);
     Viterbi(transition, pi, durations, pdf, delta, psi_duration, psi_state,
             min_duration, nobs);
+    cout << "Delta last column" << endl;
     cout << delta.col(nobs - 1) << endl;
-    viterbiPath(psi_duration, psi_state, delta);
+    ivec viterbiStates, viterbiDurations;
+    viterbiPath(psi_duration, psi_state, delta, viterbiStates,
+            viterbiDurations);
+
+    cout << "Viterbi states and durations" << endl;
+    cout << join_horiz(viterbiStates, viterbiDurations) << endl;
 
     // Debug
-    vec debug = {3, 1, 2, 3, 0, 1, 1, 0, 2, 1};
     double real_acum = 1.0;
     double debug_acum = 1.0;
+    int differences = 0;
     for(int t = 0; t < nobs; t++) {
-        real_acum *= gaussianpdf(toy_obs(t) - toy_states(t), 0, 0.1) * 0.25;
-        debug_acum *= gaussianpdf(toy_obs(t) - debug(t), 0, 0.1) * 0.25;
+        real_acum *= gaussianpdf(samples(0, t) -
+                hiddenStates(t), 0, 0.1) * 0.25;
+        debug_acum *= gaussianpdf(samples(0, t) -
+                viterbiStates(t), 0, 0.1) * 0.25;
+        if (hiddenStates(t) != viterbiStates(t))
+            differences++;
     }
-    cout << "Real: " << real_acum << " Debug: " << debug_acum << endl;
-    // TODO: Test the alpha and beta recursions with this example.
+    cout << "Real seq: " << real_acum << " Viterbi seq " << debug_acum <<
+            " Differences: " << differences << endl;
+
+    // TODO: Test the beta recursions with this example.
     // TODO: Test with a duration greater than 1.
-    // TODO: implement logs in the Viterbi algorithm.
     return 0;
 }
