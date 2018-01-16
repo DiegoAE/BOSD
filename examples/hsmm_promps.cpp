ce71c1705c9893471be5d93b4f7150f7389177c4
diego <aleagues@gmail.com>
2018-01-16 13:07:57 -0500

    Adding the basic structure required to implement the ProMP emissions.

--- examples/hsmm_promps.cpp
+++ examples/hsmm_promps.cpp
@@ -9,6 +9,37 @@ using namespace hsmm;
 using namespace robotics;
 using namespace std;
 
+class ProMPsEmission : public AbstractEmission {
+    public:
+        ProMPsEmission(vector<FullProMP> v) : AbstractEmission(promps_.size(),
+                promps_.at(0).get_num_joints()), promps_(v) {
+            // TODO: e.g. all have the same number of joints.
+        }
+
+        ProMPsEmission* clone() const {
+            return new ProMPsEmission(*this);
+        }
+
+        double loglikelihood(int state, const arma::mat& obs) const {
+            // TODO.
+            return 0.0;
+        }
+
+        void reestimate(int min_duration, const arma::cube& eta,
+                const arma::mat& obs) {
+            // TODO
+            return;
+        }
+
+        mat sampleFromState(int state, int size) const {
+            // TODO
+            return zeros<mat>(10, 10);
+        }
+
+    private:
+        vector<FullProMP> promps_;
+};
+
 int main() {
     // Creating a basic ProMP with the desired parameters
     vec mu_w {0, 0, 0, 0};
