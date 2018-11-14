1190eb6e334453d6bd70eb16b4ac55421f69a78c
Diego <dagudelo@tue.mpg.de>
2018-11-14 22:04:14 +0100

    Segmenting the observations according to the viterbi labels

--- examples/NN_standard_fit.cpp
+++ examples/NN_standard_fit.cpp
@@ -18,9 +18,11 @@ int main(int argc, char *argv[]) {
         ("help,h", "Produce help message")
         ("input,i", po::value<string>(), "Path to the input obs")
         ("viterbi,v", po::value<string>(), "Path to the input viterbi file")
+        ("nstates,s", po::value<int>(), "Number of states (NNs)")
         ("nfiles,n", po::value<int>()->default_value(1),
                 "Number of input files to process");
-    vector<string> required_fields = {"input", "viterbi", "nfiles"};
+    vector<string> required_fields = {"input", "viterbi", "nfiles",
+            "nstates"};
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
@@ -37,6 +39,9 @@ int main(int argc, char *argv[]) {
     string input_filename = vm["input"].as<string>();
     string viterbi_filename = vm["viterbi"].as<string>();
     int nseq = vm["nfiles"].as<int>();
+    int nstates = vm["nstates"].as<int>();
+    vector<mat> obs_for_each_state[nstates];
+    // TODO: generate the time indexes.
     for(int i = 0; i < nseq; i++) {
         string iname = input_filename;
         string vname = viterbi_filename;
@@ -48,9 +53,17 @@ int main(int argc, char *argv[]) {
         obs.load(iname, raw_ascii);
         imat vit;
         vit.load(vname, raw_ascii);
-        cout << obs.n_rows << " " << obs.n_cols << endl;
-        cout << vit.n_rows << " " << vit.n_cols << endl;
+        ivec hs = vit.col(0);
+        ivec dur = vit.col(1);
+        int idx = 0;
+        for(int j = 0; j < dur.n_rows; j++) {
+            mat segment = obs.cols(idx, idx + dur(j) - 1);
+            obs_for_each_state[hs(j)].push_back(segment);
+            idx += dur(j);
+        }
     }
+    for(int i = 0; i < nstates; i++)
+        cout << obs_for_each_state[i].size() << endl;
     int njoints = 3;
     int hidden_units = 10;
     FFN<MeanSquaredError<>, RandomInitialization> neural_network;
