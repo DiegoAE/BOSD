f98df40408cb79acb0de624bbb3bbee388626056
Diego Agudelo-Espana <dagudelo@tue.mpg.de>
2020-02-19 17:14:30 +0100

    Evaluation likelihood with the Cox model

--- baselines/cox_ph.py
+++ baselines/cox_ph.py
@@ -1,11 +1,27 @@
 import argparse
 
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 from lifelines import CoxPHFitter
 
 from data.ecg import obs_ecg, gt_vit_seq_ecg
 
+MAX_DUR = 180
+
+def loglikelihood(hazards, cum_hazards):
+    """ Refer to https://stats.stackexchange.com/questions/417303/
+    what-is-the-likelihood-for-this-process"""
+    assert hazards.shape == cum_hazards.shape
+    lls = np.log(hazards) - cum_hazards
+    return lls
+
+def get_hazard_from_cum_hazard(cum_hazard):
+    """
+        Refer to the Discrete survival models section in lifelines.
+    """
+    hazard = 1 - np.exp(cum_hazard[:-1,:] - cum_hazard[1:,:])
+    return hazard
 
 def get_covariates_dict_from_list(covs_list):
     d = {}
@@ -42,15 +58,29 @@ def fit_cph(data):
     cph = CoxPHFitter()
     cph.fit(data, duration_col='survival_time', event_col='finished',
             strata=['state'])
-    cph.print_summary()
+    #cph.print_summary()
+    return cph
 
 
 if __name__ == '__main__':
-    cph = CoxPHFitter()
     for nobs in [5, 10, 20, 40]:
         print(nobs)
         ecg_pd = get_ecg_pd(nobs)
         s0_pd = ecg_pd.loc[~ecg_pd['state'].astype(bool)]
         s1_pd = ecg_pd.loc[ecg_pd['state'].astype(bool)]
-        fit_cph(s0_pd)
-        fit_cph(s1_pd)
+        s0_cph = fit_cph(s0_pd)
+        s1_cph = fit_cph(s1_pd)
+        test = s1_pd.loc[:10]
+        times_to_predict = np.arange(MAX_DUR + 1)
+        cumulative_hazard = s1_cph.predict_cumulative_hazard(test,
+                times=times_to_predict)
+        print('Debug', times_to_predict.shape)
+        cumulative_hazard = cumulative_hazard.values
+        hazard = get_hazard_from_cum_hazard(cumulative_hazard)
+
+        #plt.plot(times_to_predict[:-1], hazard)
+        #plt.show()
+
+        lls = loglikelihood(hazard, cumulative_hazard[:-1,:])
+        plt.plot(times_to_predict[:-1], lls)
+        plt.show()
