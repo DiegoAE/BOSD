125a1e2ff2066f1017fa8f6845f6ced7165214d9
Diego Agudelo-Espana <dagudelo@tue.mpg.de>
2020-06-19 09:00:33 +0200

    Adding KM as a baseline method and Brier score as performance measure

--- baselines/cox_ph.py
+++ baselines/cox_ph.py
@@ -6,15 +6,13 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import pandas as pd
-from lifelines import CoxPHFitter
+from lifelines import CoxPHFitter, KaplanMeierFitter
 
-
-MAX_DUR = 180
 
 def get_pmf_from_survival(survival_f):
     pmf = survival_f.copy()
     for i in range(survival_f.shape[0] - 1):
-        pmf[i, :] -= pmf[i + 1, :]
+        pmf[i] -= pmf[i + 1]
     sums = np.sum(pmf, axis=0)
 
     #plt.plot(np.arange(MAX_DUR), pmf)
@@ -48,7 +46,9 @@ def get_state_sequence_and_residual_time_from_vit(vit):
     residual_times = []
     for hs, dur in vit:
         states.extend([hs] * dur)
-        residual_times.extend(np.arange(dur)[::-1])
+
+        # Note that we're shifting by one the residual times here.
+        residual_times.extend(np.arange(dur)[::-1] + 1)
     assert len(states) == len(residual_times) and len(states) == vit[:,1].sum()
     return states, residual_times
 
@@ -77,11 +77,39 @@ def get_pd(horizon, lobs, lvit, ntest_obs=None):
     return pd.DataFrame(data_dict)
 
 def fit_cph(data):
-    cph = CoxPHFitter()
-    cph.fit(data, duration_col='survival_time', event_col='complete')
-    # cph.print_summary()
     return cph
 
+def fit_km(training_data, test_data, times_to_predict):
+    kmf = KaplanMeierFitter()
+    kmf.fit(training_data['survival_time'])
+    survival_f = kmf.survival_function_at_times(times_to_predict)
+    pmf = get_pmf_from_survival(
+            kmf.survival_function_at_times(times_to_predict).values)
+    ll = 0
+    gt = test_data['survival_time'].values
+    for l in gt:
+        ll += np.log(pmf[l])
+    one_hot_gt = np.eye(len(pmf))[gt]
+    #upmf = np.array([1./len(pmf)] * len(pmf))
+    return ll, brier_score(pmf, one_hot_gt), np.log(1./len(pmf)) * len(gt)
+
+def fit_cph(train_pd, test_pd, times_to_predict):
+    cph = CoxPHFitter()
+    cph.fit(train_pd, duration_col='survival_time', event_col='complete')
+    # cph.print_summary()
+    survival_f = cph.predict_survival_function(test_pd,
+            times=times_to_predict)
+    survival_f = survival_f.values
+    pmfs = get_pmf_from_survival(survival_f)
+    ll = 0
+    gt = test_pd['survival_time'].values
+    for pmf,l in zip(pmfs.T, gt):
+        ll += np.log(pmf[l])
+    one_hot_gt = np.eye(len(pmf))[gt]
+    return ll, brier_score(pmfs.T, one_hot_gt), np.log(1./len(pmf)) * len(gt)
+
+def brier_score(pmf, one_hot_gt):
+    return np.sum(np.square(pmf - one_hot_gt)) / len(one_hot_gt)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
@@ -115,31 +143,46 @@ if __name__ == '__main__':
     test_obs = np.loadtxt(args.testing_obs, ndmin=2)
     test_vit = np.loadtxt(args.testing_vit).astype('int')
     test_pd = get_pd(args.horizon, [test_obs], [test_vit], args.ntest_obs)
+    print('Number of test predictions:',  len(test_pd['survival_time']))
 
-    lls = []
+    lls_km = []
+    bss_km = []
+    rgs = []
+
+    lls_cph = []
+    bss_cph = []
+    rgs_cph = []
     for leave_one_out in range(args.nfiles):
         lobs_copy = list(lobs)
         lvit_copy = list(lvit)
         lobs_copy.pop(leave_one_out)
         lvit_copy.pop(leave_one_out)
 
+        # Note the +1 since the min residual value is 1.
+        times_to_predict = np.arange(181)
+
         # Training pandas data frame.
         train_pd = get_pd(args.horizon, lobs_copy, lvit_copy)
 
-        # Learning Cox's proportional hazards model.
-        cph = fit_cph(train_pd)
-        times_to_predict = np.arange(MAX_DUR)
-        survival_f = cph.predict_survival_function(test_pd,
-                times=times_to_predict)
-        survival_f = survival_f.values
-        pmf = get_pmf_from_survival(survival_f)
-        ll = 0
-        terms = 0
-        gt_survival_times = test_pd['survival_time'].values
-        for i in range(len(gt_survival_times)):
-            terms += 1
-            ll += np.log(pmf[gt_survival_times[i], i])
-        print('iter {}, test terms: {}'.format(leave_one_out, terms))
-        lls.append(ll)
-    print('test loglikelihood: {} +- {}'.format(np.mean(lls), np.std(lls)))
+        # Fitting KM estimator.
+        ll_km, bs_km, rg = fit_km(train_pd, test_pd, times_to_predict)
+        lls_km.append(ll_km)
+        bss_km.append(bs_km)
+        rgs.append(rg)
 
+        # Fitting Cox's proportional hazards model.
+        ll_cph, bs_cph, rg = fit_cph(train_pd, test_pd, times_to_predict)
+        lls_cph.append(ll_cph)
+        bss_cph.append(bs_cph)
+        rgs_cph.append(rg)
+
+        print('Iter {}. KM ll: {}. CPH ll: {}'.format(leave_one_out, ll_km, ll_cph))
+
+    print('KM.\n\tll: {} +- {}\n\tbs: {} +- {}\n\trg {} +- {}'.format(
+        np.mean(lls_km), np.std(lls_km),
+        np.mean(bss_km), np.std(bss_km),
+        np.mean(rgs), np.std(rgs)))
+    print('CPH.\n\tll: {} +- {}\n\tbs: {} +- {}\n\trg {} +- {}'.format(
+        np.mean(lls_cph), np.std(lls_cph),
+        np.mean(bss_cph), np.std(bss_cph),
+        np.mean(rgs_cph), np.std(rgs_cph)))
